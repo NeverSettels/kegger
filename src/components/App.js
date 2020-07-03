29496cf23fe34e0129762b3c99a5701648a053c8
NeverSettels <settels.eric@gmail.com>
2020-07-03 09:40:26 -0700

    dependency installation scaring me saftey commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BreweryControl from "./BreweryControl"
 function App() {
   return (
     <div className="App">
       <BreweryControl />
+
     </div>
   );
 }
---
+++ src/components/BreweryControl.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class BreweryControl extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterKegList: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
