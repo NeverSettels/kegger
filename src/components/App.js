b8e4f2e033e7c20cc1f2d861dc5c474a2ab8331c
NeverSettels <settels.eric@gmail.com>
2020-07-03 09:09:25 -0700

    react taking years to npm install so time commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BreweryControl from "./BreweryControl"
+function App() {
+  return (
+    <div className="App">
+      <BreweryControl />
+    </div>
+  );
+}
+
+export default App;
